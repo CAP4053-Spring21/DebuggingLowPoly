acb45af86120833e8f81599cabe7775ab3f46a1b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-03-05 09:29:06 -0500

    Finished Main Menu and most of it functionality

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        int scene_index = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(scene_index + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Debug.Log("QUIT GAME");
+        Application.Quit();
     }
 }
+
