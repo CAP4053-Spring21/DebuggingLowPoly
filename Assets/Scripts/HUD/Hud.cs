1cce710f0ebd2441c4bc74e6d0abdfb402152e69
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-03-23 20:14:24 -0400

    Resume game button and save and quit button works

--- Assets/Scripts/HUD/Hud.cs
+++ Assets/Scripts/HUD/Hud.cs
@@ -2,30 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class Hud : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject minimap;
     public bool ispaused;
 
-    public void toggle()
+    public void togglePause()
     {
         minimap.SetActive(ispaused);
 
-        ispaused = !ispaused;
-
         // Toggles the pause menu
-        pauseMenu.SetActive(ispaused);
+        pauseMenu.SetActive(!ispaused);
+
+        // Freezes the game.
+        Time.timeScale = (ispaused) ? 1 : 0;
+        ispaused = !ispaused;
+    }
+
+    public void saveQuit()
+    {
+        int scene_index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene_index - 1);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F2) || Input.GetKeyDown(KeyCode.F3)) 
         {
-            // Freezes the game.
-            Time.timeScale = (ispaused) ? 1 : 0;
-            
-            toggle();
+            togglePause();
         }
     }
 }
