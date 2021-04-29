867816a915c7675dee3a582bd342d7811e1a796b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-04-29 15:33:54 -0400

    Collecting food should affect health points

--- Assets/Scripts/CollectableScript.cs
+++ Assets/Scripts/CollectableScript.cs
@@ -8,12 +8,24 @@ public class CollectableScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource collectSound;
+    public Unit unit;
+    public GameObject prompt;
 
     void OnTriggerEnter(Collider other)
     {
         collectSound.Play();
-        Destroy(gameObject);
 
+
+        if (unit.currentHP <  unit.maxHP)
+        {
+            int hpDiff = unit.maxHP - unit.currentHP;
+            unit.currentHP += (hpDiff >= 15) ? 15 : hpDiff;
+            Destroy(gameObject);
+        }
+        else
+        {
+            prompt.SetActive(true);
+        }
         
     }
 
