9f5a5cb4307b68f754d3bc55cf57653b113f3af6
muditjains <muditjains@knights.ucf.edu>
2021-04-30 16:51:48 -0400

    Sharks moving around

--- Assets/SharkScript.cs
+++ Assets/SharkScript.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class SharkScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    int counter;
+    int x, y, z;
     void Start()
     {
-        
+         counter = 0;
+        x = 0;
+        y = 0;
+        z = 1;
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.5f, 0f, 0f);
-        transform.Translate(0f,0f, 0.5f);
+        transform.Translate(x, y, z);
+        counter = counter + 1;
+        if (counter == 200)
+        {
+            transform.Rotate(0, 180, 0);
+            counter = 0;
+
+        }
+
+
+
     }
 }
